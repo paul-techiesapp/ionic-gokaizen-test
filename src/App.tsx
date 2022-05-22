617dedd926e40fd816e4f7126128515180877663
Paul Lee <paul@techiesapp.io>
2022-05-22 23:44:39 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,111 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
+  IonButton,
+  IonContent,
   IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
   IonLabel,
+  IonList,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { ellipse, square, triangle } from "ionicons/icons";
+import Tab1 from "./pages/Tab1";
+import Tab2 from "./pages/Tab2";
+import Tab3 from "./pages/Tab3";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-
+import "./theme/variables.css";
+import { BLE } from "@awesome-cordova-plugins/ble";
+import { useState } from "react";
+const { stringToBytes } = require("convert-string");
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [devices, setDevices] = useState<any[]>([]);
+
+  const connectBle = async (deviceId: string) => {
+    try {
+      BLE.connect(deviceId).subscribe(async (e) => {
+        const res = await BLE.requestMtu(deviceId, 400);
+        console.log(e);
+        const writeRes = await BLE.writeWithoutResponse(
+          deviceId,
+          "f000ffc0-0451-4000-b000-000000000000",
+          "f000ffc1-0451-4000-b000-000000000000",
+          stringToBytes("5AA5AA00130000691D0D0A")
+        );
+        console.log("writeRes", writeRes);
+      });
+    } catch (e) {
+      console.log("errors", e);
+    }
+  };
+  const scan = async () => {
+    try {
+      console.log("checkk");
+      BLE.scan([], 4).subscribe((peripheral) => {
+        if (peripheral && peripheral.name) {
+          console.log(peripheral);
+          const list = devices;
+          list.push(peripheral);
+          // console.log("list");
+          setDevices(list);
+        }
+      });
+      // console.log(res._subscribe);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  console.log("device", devices);
+  return (
+    <IonApp>
+      <IonContent>
+        <IonButton onClick={() => scan()}>Scan</IonButton>
+
+        {/*-- List of Text Items --*/}
+        <IonList>
+          {devices.map((device, index) => (
+            <IonItemSliding key={`device-${index}`}>
+              <IonItem>
+                <IonLabel>{device.name}</IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption onClick={() => connectBle(device.id)}>
+                  connect
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,73 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { BLE } from "@awesome-cordova-plugins/ble";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { useEffect, useState } from "react";
+import { connect } from "http2";
+// import ConvertString from "convert-string";
 
 const Tab1: React.FC = () => {
+  const [devices, setDevices] = useState<any[]>([]);
+  const convertStringToBytes = (str: string) => {
+    const myBuffer: any = [];
+    const buffer = new Buffer(str, "utf16le");
+    for (var i = 0; i < buffer.length; i++) {
+      myBuffer.push(buffer[i]);
+    }
+
+    return myBuffer;
+  };
+  const connectBle = async (deviceId: string) => {
+    try {
+      BLE.connect(deviceId).subscribe(async (e) => {
+        const res = await BLE.requestMtu(deviceId, 400);
+        console.log(e);
+        BLE.writeWithoutResponse(
+          deviceId,
+          "f000ffc0-0451-4000-b000-000000000000",
+          "f000ffc1-0451-4000-b000-000000000000",
+          convertStringToBytes("5AA5AA00130000691D0D0A")
+        );
+      });
+    } catch (e) {
+      console.log("errors", e);
+    }
+  };
+  const scan = async () => {
+    try {
+      console.log("checkk");
+      BLE.scan([], 4).subscribe((peripheral) => {
+        if (peripheral && peripheral.name) {
+          console.log(peripheral);
+          const list = devices;
+          list.push(peripheral);
+          // console.log("list");
+          setDevices(list);
+        }
+      });
+      // console.log(res._subscribe);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  // useEffect(() => {
+  //   // check();
+  // }, []);
+
+  console.log("devicess", devices);
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +81,24 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {/* <ExploreContainer name="Tab 1 page" /> */}
+        <IonButton onClick={() => scan()}>Scan</IonButton>
+
+        {/*-- List of Text Items --*/}
+        <IonList>
+          {devices.map((device, index) => (
+            <IonItemSliding key={`device-${index}`}>
+              <IonItem>
+                <IonLabel>{device.name}</IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption onClick={() => connectBle(device.id)}>
+                  connect
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
